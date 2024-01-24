fdce6df3b0d2241f12538139aaabb816b23715ce
claytonknittel <cknittel@wustl.edu>
2024-01-23 23:39:14 -0800

    wave eqn

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,35 @@
 mod window;
 
-fn main() {
-  let mut window = window::Window::new(10, 10);
+use std::io::Write;
+use termion::async_stdin;
+use termion::cursor::HideCursor;
+use termion::event::{Event, Key, MouseEvent};
+use termion::input::{MouseTerminal, TermRead};
+use termion::raw::IntoRawMode;
 
-  for _ in 0..10 {
-    window.render();
-    std::thread::sleep(std::time::Duration::from_millis(200));
+fn main() {
+  let stdout = HideCursor::from(MouseTerminal::from(
+    std::io::stdout().lock().into_raw_mode().unwrap(),
+  ));
+  let mut window = window::Window::new(stdout, 60, 40);
+  let mut stdin = async_stdin().events();
+
+  'outer: loop {
+    while let Some(evt) = stdin.next() {
+      match evt {
+        Ok(Event::Key(Key::Char('q'))) => break 'outer,
+        Ok(Event::Mouse(me)) => match me {
+          MouseEvent::Press(_, x, y) | MouseEvent::Hold(x, y) => {
+            window.click(x as u32, y as u32);
+          }
+          _ => (),
+        },
+        Err(_) => break 'outer,
+        _ => {}
+      }
+    }
+    std::thread::sleep(std::time::Duration::from_millis(20));
+    window.advance();
+    window.render().unwrap();
   }
 }
--- src/window.rs
+++ src/window.rs
@@ -1,46 +1,111 @@
 use std::io::Write;
 use termion::{color, cursor};
 
-pub struct Window {
-  width: u32,
-  height: u32,
-  x: u32,
+const C: f32 = 0.1;
+
+#[derive(Clone, Copy)]
+pub struct Particle {
+  pos: f32,
+  vel: f32,
 }
 
-impl Window {
-  pub fn new(width: u32, height: u32) -> Self {
-    let s = Self {
+impl Particle {
+  fn new() -> Self {
+    Self { pos: 0., vel: 0. }
+  }
+
+  /// Left, Up, Right, Down
+  fn perturb(&self, neighbors: (Particle, Particle, Particle, Particle)) -> Self {
+    let ddx = neighbors.2.pos + neighbors.0.pos - 2. * self.pos;
+    let ddy = neighbors.3.pos + neighbors.1.pos - 2. * self.pos;
+    let vel = self.vel + (C * C) * (ddx + ddy);
+    let pos = self.pos + C * self.vel;
+    Self { pos, vel }
+  }
+}
+
+pub struct Window<W: Write> {
+  stdout: W,
+  width: u32,
+  height: u32,
+  grid: Vec<Particle>,
+}
+
+impl<W: Write> Window<W> {
+  pub fn new(stdout: W, width: u32, height: u32) -> Self {
+    let mut s = Self {
+      stdout,
       width,
       height,
-      x: 0,
+      grid: vec![Particle::new(); (width * height) as usize],
     };
-    s.allocate();
+    s.allocate().expect("Failed to initialize window");
     s
   }
 
-  fn allocate(&self) {
-    for _ in 0..self.height {
-      println!();
+  pub fn stdout(&mut self) -> &mut W {
+    &mut self.stdout
+  }
+
+  pub fn get(&self, x: i32, y: i32) -> Particle {
+    let x = x.clamp(0, self.width as i32 - 1) as u32;
+    let y = y.clamp(0, self.height as i32 - 1) as u32;
+    self.grid[(x + y * self.width) as usize]
+  }
+
+  pub fn get_mut(&mut self, x: u32, y: u32) -> &mut Particle {
+    debug_assert!(x < self.width);
+    debug_assert!(y < self.height);
+    &mut self.grid[(x + y * self.width) as usize]
+  }
+
+  pub fn click(&mut self, x: u32, y: u32) {
+    if 1 <= x && x <= self.width && 1 <= y && y <= self.height {
+      self.get_mut(x - 1, y - 1).pos = 1.;
     }
   }
 
-  pub fn render(&mut self) {
-    print!("{}", cursor::Up(self.height as u16));
+  fn allocate(&mut self) -> std::io::Result<()> {
+    write!(self.stdout, "{}{}", termion::clear::All, cursor::Goto(1, 1))
+  }
+
+  pub fn advance(&mut self) {
+    self.grid = (0..self.height as i32)
+      .flat_map(|y| (0..self.width as i32).map(move |x| (x, y)))
+      .map(|(x, y)| {
+        self.get(x, y).perturb((
+          self.get(x - 1, y),
+          self.get(x, y - 1),
+          self.get(x + 1, y),
+          self.get(x, y + 1),
+        ))
+      })
+      .collect();
+  }
+
+  pub fn render(&mut self) -> std::io::Result<()> {
+    write!(self.stdout, "{}", cursor::Up(self.height as u16))?;
     for y in 0..self.height {
       for x in 0..self.width {
-        if x + y * self.width == self.x {
-          print!(
-            "{}X{}",
-            color::Fg(color::Rgb(255, 0, 255)),
-            color::Fg(color::Reset)
-          );
-        } else {
-          print!(".");
-        }
+        let particle = self.get(x as i32, y as i32);
+        write!(
+          self.stdout,
+          "{}O",
+          color::Fg(color::Rgb(
+            (particle.pos * 128.).clamp(0., 128.) as u8,
+            ((1. - particle.pos) * 128.).clamp(0., 128.) as u8,
+            128
+          ))
+        )?;
       }
-      print!("{}{}", cursor::Left(self.width as u16), cursor::Down(1));
+      write!(
+        self.stdout,
+        "{}{}{}",
+        color::Fg(color::Reset),
+        cursor::Left(self.width as u16),
+        cursor::Down(1)
+      )?;
     }
-    std::io::stdout().flush();
-    self.x += 1;
+    self.stdout.flush()
   }
 }
